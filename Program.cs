183b754c2146fcb7b199ae4939c4f23594e9ae06
shunjid <shunjid1862@gmail.com>
2020-09-27 19:35:46 +0600

    initiating pattern

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace design_pattern
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var pie = int.Parse(Console.ReadLine() ?? "3.14156");
+            PrintLn($"Value of pie is: {pie}");
+        }
+        
+        private static void PrintLn(object anyObject)
+        {
+            Console.WriteLine(anyObject);
+        }
+    }
+}
