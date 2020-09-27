4cdf2096d403ee0b6addf36ef2ce509d95174049
shunjid <shunjid1862@gmail.com>
2020-09-28 00:47:18 +0600

    added documentation to Singleton

--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -2,17 +2,26 @@ using System;
 
 namespace design_pattern.Singleton
 {
+    //
+    // Class: Singleton
+    // Implements singleton design pattern.
+    // Keyword "sealed" restricts other class to inherit this class.
+    //
     public sealed class Singleton
     {
-        private static int _counter = 0;
-        private static Singleton _singleton = null;
-
+        private static int _counter;
+        private static Singleton _singleton;
+        
+        // Private constructor ensures publicly unavailability
+        // and usage only by the Singleton class.
         private Singleton()
         {
             ++_counter;
             Console.WriteLine($"Counter is now: {_counter}");
         }
-
+        
+        // Publicly available getter to provide instance only once
+        // and leveraging privately assigned property for rest of the time.
         public static Singleton GetSingleton
         {
             get
